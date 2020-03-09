e85995d65626c61378032dbae3e9a77b17aedba3
Feby Lailani <febylailani@Febys-MacBook-Pro.local>
2020-03-09 10:19:06 +0700

    Update Content
    -Add image content
    -Add label/content title

--- NC_1/ViewController.swift
+++ NC_1/ViewController.swift
@@ -13,70 +13,45 @@ class ViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var picture1: UIButton!
     @IBOutlet weak var picture2: UIButton!
+    @IBOutlet weak var picture3: UIButton!
+    @IBOutlet weak var picture4: UIButton!
     @IBOutlet weak var labelName1: UILabel!
     @IBOutlet weak var labelName2: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var smallerPicture1: UIButton!
-    @IBOutlet weak var smallerPicture2: UIButton!
-    @IBOutlet weak var labelSP1: UILabel!
-    @IBOutlet weak var labelSP2: UILabel!
+    @IBOutlet weak var labelName3: UILabel!
+    @IBOutlet weak var labelName4: UILabel!
+    @IBOutlet var mainView: UIView!
     
-//    var searchValue = ""
-//    var data = [label1, label2, labelSP1, labelSP2] // my data here.
-//    var filteredData: ["String"]!
-//
+
     override func viewDidLoad() {
          super.viewDidLoad()
+        
         culinarySetup()
-        recreationSetup()
-        museumSetup()
-        cultureSetup()
-         // function masing-masing segmen
-//        filteredData = data
-        searchInput.returnKeyType = .search //untuk button return key
-        searchInput.delegate = self
-        
-        
-//        let text = "hello doni"
-//        let search = "hello Doni"
-//
-//        if (search == text) {
-//
-//        }else{hasilnya masuk ke else, krn dicek per string, text dan search beda karena huruf D pada search merupakan kapital
-//
-//        }
-        
+//        recreationSetup()
+//        museumSetup()
+//        cultureSetup()
     }
-    
-    
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)  {
-//        searchBar.resignFirstResponder()
-//        searchValue = searchBar.text ?? "" //pemberian default value utk optional
-//        print(searchValue) // untuk mendapatkan keyword
-//        //input filternya berdasarkan searchValue
-//    }
-//
-//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-//        searchValue = ""
-//    }
-    
     
     @IBAction func segmentedView(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
-            print("Culinary")
+            
             culinarySetup()
+            mainView.layer.backgroundColor = UIColor(red:122/255, green: 255/255, blue: 200/255, alpha: 1.0).cgColor
         }
         if sender.selectedSegmentIndex == 1 {
-            print("Recreation")
+            
             recreationSetup()
+            mainView.layer.backgroundColor = UIColor(red:95/255, green: 245/255, blue: 228/255, alpha: 0.96).cgColor
+            
         }
         if sender.selectedSegmentIndex == 2 {
-            print("Museum")
+        
             museumSetup()
+            mainView.layer.backgroundColor = UIColor(red:115/255, green: 232/255, blue: 255/255, alpha: 1.0).cgColor
         }
         if sender.selectedSegmentIndex == 3 {
-            print("Culture")
+       
             cultureSetup()
+            mainView.layer.backgroundColor = UIColor(red:158/255, green: 190/255, blue: 255/255, alpha: 1.0).cgColor
         }
         
     }
@@ -85,39 +60,62 @@ class ViewController: UIViewController, UISearchBarDelegate {
     
     func culinarySetup() {
         label1.text = "Try Our Local Food!"
-        label2.text = "Looking for Our Signature?"
+        labelName1.text = "Batagor"
+        labelName2.text = "Rujak"
+        labelName3.text = "Surabi asin"
+        labelName4.text = "Surabi manis"
+        
         picture1.setImage(UIImage(named: "Batagor"), for: UIControl.State.normal)
         picture2.setImage(UIImage(named: "Rujak"), for: UIControl.State.normal)
-        smallerPicture1.setImage(UIImage(named: "Surabi asin"), for: UIControl.State.normal)
-        smallerPicture2.setImage(UIImage(named: "Surabi manis"), for: UIControl.State.normal)
+        picture3.setImage(UIImage(named: "Surabi asin"), for: UIControl.State.normal)
+        picture4.setImage(UIImage(named: "Surabi manis"), for: UIControl.State.normal)
     }
+    
     func recreationSetup() {
-        label1.text = "Welcome to Bandung City!"
-        label2.text = "Ready to Blend with Nature?"
-        picture1.setImage(UIImage(named: "mie yamin"), for: UIControl.State.normal)
-        picture2.setImage(UIImage(named: "nasi timbel"), for: UIControl.State.normal)
+        label1.text = "Let's Get Ready to Explore!"
+        labelName1.text = "Bugbrug Waterfall"
+        labelName2.text = "Sukawana Tea Plantation"
+        labelName3.text = "Aseupan Waterfall"
+        labelName4.text = "Cihideung Valley"
+        
+        picture1.setImage(UIImage(named: "Bugbrug"), for: UIControl.State.normal)
+        picture2.setImage(UIImage(named: "Sukawana"), for: UIControl.State.normal)
+        picture3.setImage(UIImage(named: "Aseupan"), for: UIControl.State.normal)
+        picture4.setImage(UIImage(named: "Cihideung valley"), for: UIControl.State.normal)
     }
+    
     func museumSetup() {
         label1.text = "Deep Dive to Bandung!"
-        label2.text = "Looking for Collaboration Space?"
-        picture1.setImage(UIImage(named: "Batagor"), for: UIControl.State.normal)
-        picture2.setImage(UIImage(named: "Rujak"), for: UIControl.State.normal)
+        labelName1.text = "Museum Pos"
+        labelName2.text = "Museum Geologi"
+        labelName3.text = "Bandung Planning Gallery"
+        labelName4.text = "Museum KAA"
+        
+        picture1.setImage(UIImage(named: "pos"), for: UIControl.State.normal)
+        picture2.setImage(UIImage(named: "geologi"), for: UIControl.State.normal)
+        picture3.setImage(UIImage(named: "bandung planning gallery"), for: UIControl.State.normal)
+        picture4.setImage(UIImage(named: "KAA"), for: UIControl.State.normal)
     }
+    
     func cultureSetup() {
         label1.text = "How's Bandung People?"
-        label2.text = "Don't Miss It!"
-        picture1.setImage(UIImage(named: "Batagor"), for: UIControl.State.normal)
-        picture2.setImage(UIImage(named: "Rujak"), for: UIControl.State.normal)
+        labelName1.text = "Hong Community"
+        labelName2.text = "Bandung China Town"
+        labelName3.text = "Saung Udjo"
+        labelName4.text = "Traditional Puppet"
+        
+        picture1.setImage(UIImage(named: "hong"), for: UIControl.State.normal)
+        picture2.setImage(UIImage(named: "china town"), for: UIControl.State.normal)
+        picture3.setImage(UIImage(named: "udjo"), for: UIControl.State.normal)
+        picture4.setImage(UIImage(named: "puppet"), for: UIControl.State.normal)
     }
+    
     @IBAction func emergencyGuide(_ sender: UIButton) {
         let alert = UIAlertController(title: "Emergency Contact", message:
-            "Immigration (+6222) 7272081; Fire Brigade 113 - (+6222) 7207113; Red Cross +6222 4207051 or 4204052; Electrical interference +6222 5222043; Police 112 - (+6222) 4203500 or 4244444; Husein Sastranegara Airport (+6222) 6041221 or 6033889", preferredStyle: .alert)
+            "Immigration (+6222) 7272081;\nFire Brigade 113 - (+6222) 7207113;\nRed Cross +6222 4207051 or 4204052;\n Electrical interference +6222 5222043;\n Police 112 - (+6222) 4203500 or 4244444;\n Husein Sastranegara Airport (+6222) 6041221 or 6033889", preferredStyle: .alert)
         let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-    
-    
-    
 }
 
